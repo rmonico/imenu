f6387178b968e57c267b62dedf554a603d222343
Rafael Monico <rmonico1@gmail.com>
2019-09-27 18:56:56 -0300

    MenuItemBuilder: internal field names changed to became different from methods

--- imenu_builder.py
+++ imenu_builder.py
@@ -35,34 +35,36 @@ class MenuBuilder(object):
 class MenuItemBuilder(object):
 
     def __init__(self, menu_builder=None):
-        self.menu_builder = menu_builder
+        self._menu_builder = menu_builder
+        self._os_command = None
+        self._variable = None
 
     def shortcut(self, shortcut):
-        self.shortcut = shortcut
+        self._shortcut = shortcut
         return self
 
     def label(self, label):
-        self.label = label
+        self._label = label
         return self
 
     def os_command(self, os_command):
-        self.os_command = os_command
+        self._os_command = os_command
         return self
 
     def set_variable(self, variable):
-        self.variable = variable
+        self._variable = variable
         return self
 
     def create(self):
-        if self.os_command:
-            self.menu_builder.add_item(self._create_os_command_menu_item())
-        elif self.variable:
-            self.menu_builder.add_item(
+        if self._os_command:
+            self._menu_builder.add_item(self._create_os_command_menu_item())
+        elif self._variable:
+            self._menu_builder.add_item(
                 self._create_variable_setter_menu_item())
-        return self.menu_builder
+        return self._menu_builder
 
     def _create_os_command_menu_item(self):
-        return OSCommandMenuItem(self.shortcut, self.label, os_command=self.os_command)
+        return OSCommandMenuItem(self._shortcut, self._label, os_command=self._os_command)
 
     def _create_variable_setter_menu_item(self):
-        return VariableSetterMenuItem(self.shortcut, self.label, variable=self.variable)
+        return VariableSetterMenuItem(self._shortcut, self._label, variable=self._variable)
