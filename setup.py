f974b81973df8052c18e6faec2c4473ef7c54345
Rafael Monico <rmonico1@gmail.com>
2019-10-09 17:22:17 -0300

    Library packaged

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='iMenu',
+      version='0.1',
+      description='A tiny framework to help create interactive menus on command line',
+      url='https://github.com/rmonico/imenu',
+      author='Rafael Monico',
+      author_email='rmonico1@gmail.com',
+      license='GPL3',
+      packages=['imenu'],
+      zip_safe=False)
